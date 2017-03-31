d2e1aa21e2e4daa6e5b9c43f964c2161f08c5ed1
Lennard Berger <berger@mecodia.de>
2017-03-31 11:51:42 +0200

    Make the layer a component and export the module by default.

--- README.md
+++ README.md
@@ -4,11 +4,17 @@ vue-multiselect-locale-de
 This is just a very thin layer over [vue-multiselect](https://github.com/monterail/vue-multiselect), providing german translation of the library.
 
 ## Installing
-You'd install this as you would install vue-multiselect via `npm install vue-multiselect-locale-de`
+You'd install this as you would install vue-multiselect via `npm install vue-multiselect-locale-de`. This however will throw an `[invalid]` warning because a Vue.js component is exported.
 Since this is just a layer, all child properties are forwarded to `vue-multiselect`.
 
 ## Using a different dependency version
 This layer ships with `2.0-beta13` by default. If you're uncomfortable with using this version you can use [npm shrinkwrap](https://docs.npmjs.com/cli/shrinkwrap), which will do it comfortably for you.
 
 ## Assemble with webpack
+You can import the module like this:
+
+```
+import `Multiselect` from `vue-multiselect-locale-de`
+```
+
 This module assumes that you use Webpack, or any other module bundler that is capable of handling ES6 syntax. If you're following along the Vue.js road, your current Webpack setup will probably work sanely.
--- vue-multiselect-locale-de.vue
+++ vue-multiselect-locale-de.vue
@@ -1,34 +1,37 @@
+<script>
 import Multiselect from 'vue-multiselect'
 
 export default {
-  name: 'vue-multiselect-locale-de'
+  name: 'vue-multiselect-locale-de',
   extends: Multiselect,
   props: {
-  /**
-   * String to show when pointing to an option
-   * @default 'Press enter to select'
-   * @type {String}
-   */
-  selectLabel: {
-    type: String,
-    default: 'Drücken Sie die Eingabetaste um das Element auszuwählen'
-  },
-  /**
-   * String to show next to selected option
-   * @default 'Selected'
-   * @type {String}
-  */
-  selectedLabel: {
-    type: String,
-    default: 'Ausgewählt'
-  },
-  /**
-   * String to show when pointing to an alredy selected option
-   * @default 'Press enter to remove'
-   * @type {String}
-  */
-  deselectLabel: {
-    type: String,
-    default: 'Drücken Sie die Eingabetaste um das Element abzuwählen'
+    /**
+     * String to show when pointing to an option
+     * @default 'Press enter to select'
+     * @type {String}
+     */
+    selectLabel: {
+      type: String,
+      default: 'Drücken Sie die Eingabetaste um das Element auszuwählen'
+    },
+    /**
+     * String to show next to selected option
+     * @default 'Selected'
+     * @type {String}
+    */
+    selectedLabel: {
+      type: String,
+      default: 'Ausgewählt'
+    },
+    /**
+     * String to show when pointing to an alredy selected option
+     * @default 'Press enter to remove'
+     * @type {String}
+    */
+    deselectLabel: {
+      type: String,
+      default: 'Drücken Sie die Eingabetaste um das Element abzuwählen'
+    }
   }
 }
+</script>
